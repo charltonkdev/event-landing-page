c6f2f30a004ae50c733116ebe310d218e6a153b0
Smurfz <charltonk.personal@gmail.com>
2024-07-01 23:53:47 +0800

    update v2.5 remove unecessary files

--- src/components/ui/ArrowButton.jsx
+++ src/components/ui/ArrowButton.jsx
@@ -1,7 +1,8 @@
-// components/ArrowButton.jsx
+// components/ui/ArrowButton.tsx
 import { GoArrowUpRight } from 'react-icons/go';
+import { FC, PropsWithChildren } from 'react';
 
-const ArrowButton = ({ children }) => {
+const ArrowButton: FC<PropsWithChildren<{}>> = ({ children }) => {
   return (
     <button className='relative w-fit px-5 py-3 mt-10 border rounded-full hover:bg-white hover:text-black duration-500 flex justify-center pl-14 hover:pl-5 hover:pr-14 group'>
       <GoArrowUpRight className='absolute group-hover:left-[75%] left-[5px] top-[6px] bg-white duration-500 group-hover:rotate-45 text-black rounded-full text-4xl mr-3 p-1'/>
